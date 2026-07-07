3112ba53fb3a75ba6082e2cb93dc14460856ff8d
kunal0721 <kunalbamoriya984@gmail.com>
2026-07-07 20:01:37 +0530

    caching work

--- src/main/java/com/example/pagination/controller/CustomersController.java
+++ src/main/java/com/example/pagination/controller/CustomersController.java
@@ -2,9 +2,13 @@ package com.example.pagination.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +36,11 @@ public class CustomersController {
 	@GetMapping("/{id}")
 	public Customers getOne(@PathVariable Long id) {
 		return service.getOneCustomer(id);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Customers> updateCustomer(@PathVariable Long id, @RequestBody Customers cus) {
+		return ResponseEntity.status(HttpStatus.OK).body(service.updateCustomer(id, cus));
 	}
 
 }
--- src/main/java/com/example/pagination/service/CustomerService.java
+++ src/main/java/com/example/pagination/service/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.pagination.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,11 +25,27 @@ public class CustomerService {
 		Pageable pagable = PageRequest.of(page, size);
 		return repo.findAll(pagable);
 	}
-	
+
+	@Cacheable(value = "customers", key = "#id")
 	public Customers getOneCustomer(Long id) {
-		return repo.findById(id).orElseThrow(() -> new CustomerNotFound("Customer not found : "  + id));
+		return repo.findById(id).orElseThrow(() -> new CustomerNotFound("Customer not found : " + id));
+	}
+
+	@CachePut(value = "customers", key = "#id")
+	public Customers updateCustomer(Long id, Customers cus) {
+		Customers c = repo.findById(id).orElseThrow(() -> new CustomerNotFound("customer not found : " + id));
+		c.setEmail(cus.getEmail());
+		c.setFirstName(cus.getFirstName());
+		c.setLastName(cus.getLastName());
+		c.setGender(cus.getGender());
+
+		return repo.save(c);
+
 	}
 }
+
+//caching  -> hashmap -> key, value 
+//   customer | customer
 
 // 100 -> page = 0, size = 10 
 // page = 1, size = 10 => 11 to 20
